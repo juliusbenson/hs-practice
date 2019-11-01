3aa63934e0a86a3157d6434e379f2397d3c958be
juliusbenson <juliusbenson@users.noreply.github.com>
2019-11-01 10:04:53 -0500

    Add files via upload

---
+++ baby.hs
@@ -0,0 +1,168 @@
+import Data.Char
+import Data.Function
+import Data.List
+import qualified Data.Map as M
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x =
+    if x > 100
+    then x
+    else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, elem c ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe 4 = "Four"
+sayMe 5 = "Five"
+sayMe x = "Not between 1 and 5"
+
+factorial' :: (Integral a) => a -> a
+factorial' 0 = 1
+factorial' n = n * factorial' (n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors' :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors' (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
+
+first :: (a,b,c) -> a
+first (x,_,_) = x
+
+second :: (a,b,c) -> b
+second (_,y,_) = y
+
+third :: (a,b,c) -> c
+third (_,_,z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length'' :: (Num b) => [a] -> b
+length'' [] = 0
+length'' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z 
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphaNum :: Char -> Bool
+isUpperAlphaNum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x  = x : filter p xs
+    | True =     filter p xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smaller = quicksort (filter (<=x) xs)
+        bigger  = quicksort (filter (> x) xs)
+    in smaller ++ [x] ++ bigger
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+    where p x = mod x 3829 == 0
+
+sumv2 :: (Num a) => [a] -> a
+sumv2 = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+mapv2 :: (a -> b) -> [a] -> [b]
+mapv2 f xs = foldr (\x acc -> f x : acc) [] xs
+
+mapv3 :: (a -> b) -> [a] -> [b]
+mapv3 f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+sumv3 :: (Num a) => [a] -> a
+sumv3 = foldl1 (+)
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filterv2 :: (a -> Bool) -> [a] -> [a]
+filterv2 p = foldr (\x acc -> if p x then x : acc else acc) []
+
+headv2 :: [a] -> a
+headv2 = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
