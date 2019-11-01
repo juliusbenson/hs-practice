f11242c438b8805a3f8a10c00db84a09c9b4c8ad
Julius Benson <jsbenson@bensonmedical.com>
2019-11-01 11:08:34 -0500

    Completing Chapter 7

--- baby.hs
+++ baby.hs
@@ -1,7 +1,9 @@
 import Data.Char
 import Data.Function
 import Data.List
-import qualified Data.Map as M
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Geometry
 
 doubleMe x = x + x
 
@@ -165,4 +167,42 @@ last' = foldl1 (\_ x -> x)
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+phoneBook =
+    [("betty","555-2938")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ,("penny","555-2111")
+    ]
+
+-- findKey :: (Eq k) => k -> [(k,v)] -> v
+-- findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+-- findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+-- findKey key [] = Nothing
+-- findKey key ((k,v):xs) =
+--     if key == k
+--     then Just v
+--     else findKey key xs
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+-- phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+-- phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
+
+text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
 
