85144a661b1791f8ae5089ca385298e920ada8e4
Julius <juliussbenson@gmail.com>
2019-11-02 16:44:12 -0500

    up to date, in order

--- baby.hs
+++ baby.hs
@@ -282,6 +282,20 @@ sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+    let nlen = length needle
+    in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in  map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
 phoneBook =
     [("betty","555-2938")
     ,("betty","342-2492")
@@ -319,30 +333,3 @@ phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
 
 text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
 text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
-
-search :: (Eq a) => [a] -> [a] -> Bool
-search needle haystack =
-    let nlen = length needle
-    in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
-
-encode :: Int -> String -> String
-encode shift msg =
-    let ords = map ord msg
-        shifted = map (+ shift) ords
-    in  map chr shifted
-
-decode :: Int -> String -> String
-decode shift msg = encode (negate shift) msg
-
-phoneBook =
-    [("betty","555-2938")
-    ,("bonnie","452-2928")
-    ,("patsy","493-2928")
-    ]
-
-findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
-findKey key =
-    foldr (\(k,v) acc ->
-        if key == k
-        then Just v
-        else acc) Nothing
