ba66b2a36e2218d1c2cdc9bab7f2c3c447ec167b
Julius <juliussbenson@gmail.com>
2019-11-02 23:44:51 -0500

    ch 8

--- baby.hs
+++ baby.hs
@@ -5,6 +5,22 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Geometry
 
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+-- data Person = Person String String Int Float String String deriving (Show)
+data Person = Person { firstName    :: String
+                     , lastName     :: String
+                     , age          :: Int
+                     , height       :: Float
+                     , phoneNumber  :: String
+                     , flavor       :: String
+                     } deriving (Show)
+
+data Car = Car { company  :: String
+               , model    :: String
+               , year     :: Int
+               } deriving (Show)
+
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -333,3 +349,38 @@ phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
 
 text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
 text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs (x2 - x1)) * (abs (y2 - y1))
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+-- firstName :: Person -> String
+-- firstName (Person firstname _ _ _ _ _) = firstname
+
+-- lastName :: Person -> String
+-- lastName (Person _ lastname _ _ _ _) = lastname
+
+-- age :: Person -> Int
+-- age (Person _ _ age _ _ _) = age
+
+-- height :: Person -> Float
+-- height (Person _ _ _ height _ _) = height
+
+-- phoneNumber :: Person -> String
+-- phoneNumber (Person _ _ _ _ number _) = number
+
+-- flavor :: Person -> String
+-- flavor (Person _ _ _ _ _ flavor) = flavor
+
+tellCar :: Car -> String
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
