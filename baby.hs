db029d791f59ff576126a5debc22ac2e7b02464d
Julius Benson <jsbenson@bensonmedical.com>
2019-11-04 17:31:43 -0600

    more stuff

--- baby.hs
+++ baby.hs
@@ -11,15 +11,20 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 data Person = Person { firstName    :: String
                      , lastName     :: String
                      , age          :: Int
-                     , height       :: Float
-                     , phoneNumber  :: String
-                     , flavor       :: String
-                     } deriving (Show)
+--                     , height       :: Float
+--                     , phoneNumber  :: String
+--                     , flavor       :: String
+                     } deriving (Eq, Show, Read)
 
 data Car = Car { company  :: String
                , model    :: String
                , year     :: Int
                } deriving (Show)
+
+data Vector a = Vector a a a deriving (Show)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
 
 doubleMe x = x + x
 
@@ -384,3 +389,13 @@ baseRect width height = Rectangle (Point 0 0) (Point width height)
 
 tellCar :: Car -> String
 tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+vplus (Vector i j k) (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+vectMult (Vector i j k) m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+scalarMult (Vector i j k) (Vector l m n) = i*l + j*m + k*n
+
