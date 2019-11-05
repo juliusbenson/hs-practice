bc22319536de4139c8a8b6b4cc5f0261c412e12b
Julius <juliussbenson@gmail.com>
2019-11-04 23:02:04 -0600

    up to functors

--- baby.hs
+++ baby.hs
@@ -26,6 +26,57 @@ data Vector a = Vector a a a deriving (Show)
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
            deriving (Eq, Ord, Show, Read, Bounded, Enum)
 
+data LockerState = Taken | Free deriving (Show, Eq)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
+
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red ="Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+class YesNo a where
+    yesno :: a -> Bool
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+instance YesNo Bool where
+    yesno = id
+instance YesNo (Maybe a) where
+    yesno Nothing = False
+    yesno _ = True
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+type AssocList k v = [(k,v)]
+
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -398,4 +449,50 @@ vectMult (Vector i j k) m = Vector (i*m) (j*m) (k*m)
 
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 scalarMult (Vector i j k) (Vector l m n) = i*l + j*m + k*n
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = elem (name, pnumber) pbook
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left ("Locker number " ++ show lockerNumber ++ " doesn't exist!")
+        Just (state, code) -> if state /= Taken
+            then Right code
+            else Left ("Locker number " ++ show lockerNumber ++ " is already taken!")
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken,"ZD39I"))
+    ,(101,(Free,"JAH3I"))
+    ,(103,(Free,"IQSA9"))
+    ,(105,(Free,"QOTSA"))
+    ,(109,(Taken,"893JJ"))
+    ,(110,(Taken,"99292"))
+    ]
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+yesnoif :: (YesNo y) => y -> a -> a -> a
+yesnoif yesnoval yesresult noresult = if yesno yesnoval then yesresult else noresult
 
