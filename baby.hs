282b6212d7cbd2683003c439eac38852bc84c9a6
Julius <juliussbenson@gmail.com>
2019-11-08 22:38:33 -0600

    Complete Chapter 8

--- baby.hs
+++ baby.hs
@@ -68,6 +68,17 @@ instance YesNo TrafficLight where
     yesno Red = False
     yesno _ = True
 
+class Tofu t where
+    tofu :: j a -> t a j
+
+data Frank a b = Frank {frankField :: b a} deriving (Show)
+instance Tofu Frank where
+    tofu x = Frank x
+
+data Barry t k p = Barry {yabba :: p, dabba :: t k}
+instance Functor (Barry a b) where
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}
+
 type Code = String
 
 type LockerMap = Map.Map Int (LockerState, Code)
