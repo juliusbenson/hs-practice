728faaa4b84e35fee4d3e4a3ad840f49d005753e
Julius <juliussbenson@gmail.com>
2020-01-05 23:10:18 -0600

    monad stuff

--- baby.hs
+++ baby.hs
@@ -575,3 +575,27 @@ lengthCompare :: String -> String -> Ordering
 --                     in  if a == EQ then b else a
 lengthCompare x y = mappend (compare (length x) (length y)) (compare x y)
 
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+applyMaybe Nothing f = Nothing
+applyMaybe (Just x) f = f x
+
+type Birds = Int
+type Pole = (Birds,Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+-- landLeft n (left,right) = (left + n,right)
+landLeft n (left,right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+-- landRight n (left,right) = (left,right + n)
+landRight n (left,right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
+
+x -: f = f x
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
