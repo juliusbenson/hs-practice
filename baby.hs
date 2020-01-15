a042fbcb7342a8de4fbc03165f995cf3aa238c92
Julius <juliussbenson@gmail.com>
2020-01-15 13:06:39 -0600

    switching computers

--- baby.hs
+++ baby.hs
@@ -648,3 +648,21 @@ in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
 canReachIn3 :: KnightPos -> KnightPos -> Bool
 canReachIn3 start end = end `elem` in3 start
 
+-- isBigGang :: Int -> Bool
+-- isBigGang x = x > 9
+
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x > 9, "Compared gang size to 9.")
+
+applyLog :: (Monoid m) => (a,m) -> (a -> (b,m)) -> (b,m)
+applyLog (x,log) f = let (y,newLog) = f x in (y,mappend log newLog)
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food,Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
+
+
