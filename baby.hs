9ef89ba67979ac74a4f8789f5c2c1aa09f6294b4
Julius <juliussbenson@gmail.com>
2020-01-20 22:58:53 -0600

    more monad stuff

--- baby.hs
+++ baby.hs
@@ -12,6 +12,7 @@ import Control.Applicative
 import Data.Monoid
 import Control.Monad
 import Control.Monad.Writer
+import Control.Monad.State
 
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
@@ -722,3 +723,91 @@ slowCountDown 0 = do
 slowCountDown x = do
     slowCountDown (x-1)
     tell [show x]
+
+addStuff :: Int -> Int
+-- addStuff = do
+--     a <- (*2)
+--     b <- (+10)
+--     return (a+b)
+addStuff x = let
+    a = (*2) x
+    b = (+10) x
+    in a+b
+
+type Stack = [Int]
+
+-- pop :: Stack -> (Int, Stack)
+-- pop (x:xs) = (x,xs)
+
+-- push :: Int -> Stack -> ((),Stack)
+-- push a xs = ((),a:xs)
+
+-- stackManip :: Stack -> (Int, Stack)
+-- -- stackManip stack = let
+-- --     ((),newStack1) = push 3 stack
+-- --     (a ,newStack2) = pop newStack1
+-- --     in pop newStack2
+-- stackManip = do
+--     push 3
+--     a <- pop
+--     pop
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x,xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((),a:xs)
+
+stackManip :: State Stack Int
+stackManip = do
+    push 3
+    pop
+    pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+    a <- pop
+    if a == 5
+        then push 5
+        else do
+            push 3
+            push 8
+
+moreStack :: State Stack ()
+moreStack = do
+    a <- stackManip
+    if a == 100
+        then stackStuff
+        else return ()
+
+stackyStack :: State Stack ()
+stackyStack = do
+    stackNow <- get
+    if stackNow == [1,2,3]
+        then put [8,3,1]
+        else put [9,2,1]
+
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
+
+threeCoins' :: State StdGen (Bool,Bool,Bool)
+threeCoins' = do
+    a <- randomSt
+    b <- randomSt
+    c <- randomSt
+    return (a,b,c)
+
+-- m >>= f = join (fmap f m)
+
+keepSmall :: Int -> Writer [String] Bool
+keepSmall x
+    | x < 4 = do
+        tell ["keeping " ++ show x]
+        return True
+    | otherwise = do
+        tell ["removing " ++ show x]
+        return False
+
+powerset :: [a] -> [[a]]
+powerset xs = filterM (\x -> [True,False]) xs
+
