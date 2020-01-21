963a18668849a6ff303f15c2445d10f26f3fc435
Julius <juliussbenson@gmail.com>
2020-01-21 12:06:36 -0600

    monads complete

--- baby.hs
+++ baby.hs
@@ -13,6 +13,7 @@ import Data.Monoid
 import Control.Monad
 import Control.Monad.Writer
 import Control.Monad.State
+import Data.Ratio
 
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
@@ -810,4 +811,68 @@ keepSmall x
 
 powerset :: [a] -> [[a]]
 powerset xs = filterM (\x -> [True,False]) xs
+
+binSmalls :: Int -> Int -> Maybe Int
+binSmalls acc x
+    | x > 9     = Nothing
+    | otherwise = Just (acc + x)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x,"")] -> Just x
+                                _ -> Nothing
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return ((x * y):ys)
+foldingFunction (x:y:ys) "+" = return ((x + y):ys)
+foldingFunction (x:y:ys) "-" = return ((y - x):ys)
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
+
+rpn :: String -> Maybe Double
+rpn st = do
+    [result] <- foldM foldingFunction [] (words st)
+    return result
+
+inMany :: Int -> KnightPos -> [KnightPos]
+inMany x start = return start >>= foldr (<=<) return (replicate x moveKnight)
+
+canReachIn :: Int -> KnightPos -> KnightPos -> Bool
+canReachIn x start end = end `elem` inMany x start
+
+newtype Prob a = Prob { getProb :: [(a,Rational)] } deriving Show
+
+instance Functor Prob where
+    fmap f (Prob xs) = Prob $ map (\(x,p) -> (f x,p)) xs
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+    [( Prob [('a',1%2),('b',1%2)] , 1%4)
+    ,( Prob [('c',1%2),('d',1%2)] , 3%4)]
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concat $ map multAll xs
+    where multAll (Prob innerxs,p) = map (\(x,r) -> (x,p*r)) innerxs
+
+instance Applicative Prob where
+    pure = return
+    (<*>) = ap
+
+instance Monad Prob where
+    return x = Prob [(x,1%1)]
+    m >>= f = flatten (fmap f m)
+    fail _ = Prob []
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads,1%2),(Tails,1%2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads,1%10),(Tails,9%10)]
+
+flipThree :: Prob Bool
+flipThree = do
+    a <- coin
+    b <- coin
+    c <- loadedCoin
+    return (all (==Tails) [a,b,c])
 
