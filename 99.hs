8c6efbb938f50e182218f3c48a7b8abc124573a0
Julius Benson <juliussbenson@gmail.com>
2020-05-16 19:15:36 -0400

    followed hlint suggestions

--- 99.hs
+++ 99.hs
@@ -3,16 +3,19 @@ module H99 where
 import System.Random as R
 
 myLast :: (Eq a) => [a] -> a
-myLast (x:[]) = x
+myLast [x] = x
 myLast (_:xs) = myLast xs
+myLast _ = error "empty list"
 
 myButLast :: [a] -> a
-myButLast (x:_:[]) = x
+myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
+myButLast _ = error "list too short"
 
 elementAt :: [a] -> Int -> a
 elementAt (x:_) 1 = x
 elementAt (_:xs) i = elementAt xs (i - 1)
+elementAt [] _ = error "index out of range"
 
 myLength :: [a] -> Int
 myLength [] = 0
@@ -20,14 +23,11 @@ myLength (_:xs) = 1 + myLength xs
 
 myReverse :: [a] -> [a]
 myReverse [] = []
-myReverse (x:xs) = (myReverse xs) ++ [x]
+myReverse (x:xs) = myReverse xs ++ [x]
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome []     = True
-isPalindrome [x]    = True
-isPalindrome (x:xs) = if (x == (myLast xs))
-    then isPalindrome (init xs)
-    else False
+isPalindrome (x:xs) = (x == myLast xs) && isPalindrome (init xs)
 
 -- isPalindrome xs = xs == (myReverse xs)
 
@@ -39,34 +39,36 @@ flatten (List []) = []
 flatten (List (x:xs)) = flatten x ++ flatten (List xs)
 
 compress :: (Eq a) => [a] -> [a]
+compress [] = []
 compress [x] = [x]
 compress (x:xs) = if x == head xs
     then compress xs
     else x:compress xs
 
 pack :: (Eq a) => [a] -> [[a]]
+pack [] = [[]]
 pack [x] = [[x]]
 pack (x:xs) = if x == head xs
-    then (x:(head (pack xs))):(tail (pack xs))
-    else [x]:(pack xs)
+    then (x : head (pack xs)) : tail (pack xs)
+    else [x] : pack xs
 
 -- pack (x:xs) = (x:takeWhile (==x) xs):pack (dropWhile (==x) xs)
 
 encode :: (Eq a) => [a] -> [(Int,a)]
-encode xs = zip (map (length) (pack xs)) (compress xs)
+encode xs = zip (map length (pack xs)) (compress xs)
 
 data RunLength a = Single a | Multiple Int a deriving (Show)
 
 encodeModified :: (Eq a) => [a] -> [RunLength a]
 encodeModified []     = []
-encodeModified (x:xs) = if (takeWhile (==x) xs) == []
-    then (Single x):encodeModified xs
-    else (Multiple (length (takeWhile (==x) xs)) x):encodeModified (dropWhile (==x) xs)
+encodeModified (x:xs) = if null (takeWhile (== x) xs)
+    then Single x : encodeModified xs
+    else Multiple (length (takeWhile (==x) xs)) x:encodeModified (dropWhile (==x) xs)
 
 decodeModified :: [RunLength a] -> [a]
 decodeModified [] = []
-decodeModified (Single a:xs) = [a] ++ decodeModified xs
-decodeModified (Multiple n a:xs) = (take n (repeat a)) ++ decodeModified xs
+decodeModified (Single a:xs) = a : decodeModified xs
+decodeModified (Multiple n a:xs) = replicate n a ++ decodeModified xs
 
 encodeDirect :: (Eq a) => [a] -> [RunLength a]
 encodeDirect = encodeModified
@@ -77,29 +79,32 @@ dupli (x:xs) = x:x:dupli xs
 
 repli :: [a] -> Int -> [a]
 repli [] _ = []
-repli (x:xs) n = take n (repeat x) ++ repli xs n
+repli (x:xs) n = replicate n x ++ repli xs n
 
 drop' :: [a] -> Int -> [a]
 drop' [] _ = []
 drop' xs n = if 0 == mod (length xs) n
-    then (drop' (init xs) n)
-    else (drop' (init xs) n) ++ (last xs):[]
+    then drop' (init xs) n
+    else drop' (init xs) n ++ [last xs]
 
 split' :: [a] -> Int -> ([a],[a])
+split' [] _ = error "index out of range"
 split' (_:xs) 0 = ([],xs)
 split' (x:xs) n = let (t,u) = split' xs (n-1)
     in (x:t,u)
 
 slice :: [a] -> Int -> Int -> [a]
-slice (x:_)  _ 1 = x:[]
-slice (x:xs) 1 m = x:(slice xs 1 (m-1))
-slice (_:xs) n m = (slice xs (n-1) (m-1))
+slice [] _ _ = []
+slice (x:_)  _ 1 = [x]
+slice (x:xs) 1 m = x : slice xs 1 (m - 1)
+slice (_:xs) n m = slice xs (n - 1) (m - 1)
 
 rotL :: [a] -> [a]
+rotL [] = []
 rotL (x:xs) = xs ++ [x]
 
 rotR :: [a] -> [a]
-rotR xs = (last xs):(init xs)
+rotR xs = last xs : init xs
 
 rotate :: [a] -> Int -> [a]
 rotate xs 0 = xs
@@ -108,44 +113,43 @@ rotate xs n = if n >= 0
     else rotate (rotR xs) (n+1)
 
 removeAt :: Int -> [a] -> (a,[a])
+removeAt _ [] = error "index out of range"
 removeAt 1 (x:xs) = (x,xs)
 removeAt n (x:xs) = let (t,u) = removeAt (n-1) xs
     in (t,x:u)
 
 removeAt' :: Int -> [a] -> (a,[a])
+removeAt' _ [] = error "index out of range"
 removeAt' 0 (x:xs) = (x,xs)
 removeAt' n (x:xs) = let (t,u) = removeAt' (n-1) xs
     in (t,x:u)
 
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x ys 1 = x:ys
-insertAt x (y:ys) n = y:(insertAt x ys (n-1))
+insertAt x (y:ys) n = y : insertAt x ys (n - 1)
+insertAt _ _ _ = error "index out of range"
 
 range :: Int -> Int -> [Int]
 range n m = if n == m
-    then n:[]
-    else n:(range (n+1) m)
+    then [n]
+    else n : range (n + 1) m
 
-rnd_select :: [a] -> Int -> IO [a]
-rnd_select xs n = do
-    gen <- getStdGen
-    return (take n (rands xs gen))
+rndSelect :: [a] -> Int -> IO [a]
+rndSelect xs n = take n . rands xs <$> getStdGen
 
 rands :: [a] -> StdGen -> [a]
 rands xs gen = let (i, newGen) = (random gen :: (Int,StdGen))
-    in (xs!!(mod i (length xs))):(rands xs newGen)
+    in (xs !! mod i (length xs)) : rands xs newGen
 
-diff_select :: Int -> Int -> IO [Int]
-diff_select n m = rnd_select [1..m] n
+diffSelect :: Int -> Int -> IO [Int]
+diffSelect n m = rndSelect [1..m] n
 
-rnd_permu :: [a] -> IO [a]
-rnd_permu xs = do
-    gen <- getStdGen
-    return (permu xs gen)
+rndPermu :: [a] -> IO [a]
+rndPermu xs = permu xs <$> getStdGen
 
 permu :: [a] -> StdGen -> [a]
 permu [] _ = []
 permu xs gen =
     let (i, newGen) = (random gen :: (Int,StdGen))
         (t,remaining) = removeAt' (mod i (length xs)) xs
-    in t:(permu remaining newGen)
+    in t : permu remaining newGen
