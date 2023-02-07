851dcb6cbb3e87d2f27c79b37619c5e182144b0b
Julius Benson <juliussbenson@gmail.com>
2023-02-06 21:28:04 -0600

    added quick lil implementation of eratosthenes

---
+++ tHRtLMP/prime.hs
@@ -0,0 +1,23 @@
+import Data.List
+
+divides :: Integral a => a -> a -> Bool
+divides d n = rem n d == 0
+
+ld :: Integer -> Integer
+ld n = ldf 2 n
+
+ldf :: Integer -> Integer -> Integer
+ldf k n | divides k n   = k
+        | k^2 > n       = n
+        | otherwise     = ldf (k+1) n
+
+prime0 :: Integer -> Bool
+prime0 n    | n < 1     = error "not a positive integer"
+            | n == 1    = False
+            | otherwise = ld n == n
+
+-- a fun foldable sieve of eratosthenes
+-- not very safe, uses !!, prone to run out of bounds
+sieve x i = (x \\ (map (*(x !! i)) x))
+
+sub100 = foldl (sieve) [2..100] [1..25] -- for example
